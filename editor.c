f629c24f20acb48fe84b12ed90282baf54f787f0
achillelamb <achille.lambrughi98@gmail.com>
2023-07-21 23:11:05 +0200

    feat: disable ctrl commands

--- editor.c
+++ editor.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 struct termios initial;
 
@@ -12,13 +14,22 @@ void enable_raw_mode() {
   tcgetattr(STDIN_FILENO, &initial);
   atexit(disable_raw_mode);
   struct termios raw = initial;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= CS8;
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
   enable_raw_mode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
   return 0;
 }
