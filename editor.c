63541a1238057c2bc00f8a21e0ea5059236cc989
achillelamb <achille.lambrughi98@gmail.com>
2023-07-22 21:11:15 +0200

    feat: add tilde on blank lines

--- editor.c
+++ editor.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/ioctl.h>
 
 /*** defines ***/
 
@@ -14,10 +15,25 @@
 #define ESCAPE_PREFIX "\x1b["
 
 /*** data ***/
+struct Config {
+  int screenrows;
+  int screencols;
+  struct termios initial;
+};
 
-struct termios initial;
+struct Config configuration;
 
 /*** output ***/
+
+void draw_rows(int rows) {
+  int y;
+  for (y = 0; y < rows; y++) {
+    write(STDOUT_FILENO, "~", 1);
+    if (y < rows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
+  }
+}
 
 void clear_screen() {
   char clear[4] = ESCAPE_PREFIX;
@@ -31,7 +47,9 @@ void reset_cursor() {
 
 void refresh_screen() {
   clear_screen();
-  reset_cursor();  
+  reset_cursor();
+  draw_rows(configuration.screenrows);
+  reset_cursor(); 
 }
 
 /*** terminal ***/
@@ -43,15 +61,15 @@ void die(char *s) {
 }
 
 void disable_raw_mode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &initial) == -1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &configuration.initial) == -1) {
     die("tcsetattr");
   }
 }
 
 void enable_raw_mode() {
-  if (tcgetattr(STDIN_FILENO, &initial) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &configuration.initial) == -1) die("tcgetattr");
   atexit(disable_raw_mode);
-  struct termios raw = initial;
+  struct termios raw = configuration.initial;
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
   raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
   raw.c_oflag &= ~(OPOST);
@@ -70,6 +88,17 @@ char editor_read_key() {
   return c;
 }
 
+int get_window_size(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** input ***/
 
 void editor_process_keypress() {
@@ -84,8 +113,13 @@ void editor_process_keypress() {
 
 /*** init ***/
 
+void init_editor() {
+  if (get_window_size(&configuration.screenrows, &configuration.screencols) == -1) die("get_window_size");
+}
+
 int main() {
   enable_raw_mode();
+  init_editor();
   while (1) {
     refresh_screen();
     editor_process_keypress();
