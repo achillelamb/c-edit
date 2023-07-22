e1c95717ecb1e4fd866f3b0cb60235b4e82d1967
achillelamb <achille.lambrughi98@gmail.com>
2023-07-22 13:03:04 +0200

    feat: quit with Ctrl-Q

--- editor.c
+++ editor.c
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include <errno.h>
 
+/*** defines ***/
+
+#define QUIT 'q'
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios initial;
@@ -48,7 +53,7 @@ int main() {
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY(QUIT)) break;
   }
   return 0;
 }
