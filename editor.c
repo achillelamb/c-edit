858dfdadce04fee2f1e4bd63c76e3b3ef8de6118
achillelamb <achille.lambrughi98@gmail.com>
2023-07-22 12:15:41 +0200

    feat: add error handling

--- editor.c
+++ editor.c
@@ -3,33 +3,45 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 struct termios initial;
 
+void die(char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disable_raw_mode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &initial);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &initial) == -1) {
+    die("tcsetattr");
+  }
 }
 
 void enable_raw_mode() {
-  tcgetattr(STDIN_FILENO, &initial);
+  if (tcgetattr(STDIN_FILENO, &initial) == -1) die("tcgetattr");
   atexit(disable_raw_mode);
   struct termios raw = initial;
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
   raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= CS8;
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1; // the unit has a value of 100 ms
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main() {
   enable_raw_mode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
   }
   return 0;
 }
